e58c8576b38761397c9e4e40bf50c6b3fe10762c
Dan Kolansky <Dan@championsoftheweb.com>
2023-03-08 12:04:28 -0500

    calulate move velocty pt 1

--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -2,7 +2,7 @@ extends Actor
 
 func _physics_process(delta):
 	var direction: = get_direction()
-	velocity = direction * speed
+	velocity = calculate_move_velocity(direction)
 	velocity = move_and_slide(velocity)
 	
 func get_direction() -> Vector2:
@@ -10,3 +10,13 @@ func get_direction() -> Vector2:
 			Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 			-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
 	)
+
+func calculate_move_velocity(
+		linear_velocity: Vector2,
+		speed: Vector2,
+		direction: Vector2
+	) -> Vector2:
+	var new_velocity: = linear_velocity
+	new_velocity.x = speed.x * direction.x
+	new_velocity.y += gravity * get_physics_process_delta_time()
+	return direction * speed
--- src/actors/actor.gd
+++ src/actors/actor.gd
@@ -5,7 +5,7 @@ export var speed: = Vector2(300.0, 1000.0)
 export var gravity: = 100.0
 var velocity: = Vector2.ZERO
 
-func _physics_process(delta):
-	velocity.y += gravity * delta
+#func _physics_process(delta):
+	
 	#velocity.y = max(velocity.y, speed.y)
 	
